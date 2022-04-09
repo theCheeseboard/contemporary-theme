4249f5afa838c103578ce9e9a039d7e2733bf095
Victor Tran <vicr12345@gmail.com>
2022-04-09 21:19:20 +1000

    Changes to support Windows

--- demo/main.cpp
+++ demo/main.cpp
@@ -15,20 +15,20 @@ int main(int argc, char* argv[]) {
     //Get the accent colour
     QColor accentCol;
 #ifdef Q_OS_WIN
-    QSettings accentDetection("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\DWM", QSettings::NativeFormat);
-    if (accentDetection.contains("ColorizationColor")) {
-        accentCol = QColor::fromRgb(QRgb(accentDetection.value("ColorizationColor").toInt() & 0x00FFFFFF));
-        if (d->currentStyle == ContemporaryLight) {
-            pal.setColor(QPalette::Button, accentCol.lighter(150));
-        } else {
-            pal.setColor(QPalette::Button, accentCol);
-        }
-    } else {
+//    QSettings accentDetection("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\DWM", QSettings::NativeFormat);
+//    if (accentDetection.contains("ColorizationColor")) {
+//        accentCol = QColor::fromRgb(QRgb(accentDetection.value("ColorizationColor").toInt() & 0x00FFFFFF));
+//        if (d->currentStyle == ContemporaryLight) {
+//            pal.setColor(QPalette::Button, accentCol.lighter(150));
+//        } else {
+//            pal.setColor(QPalette::Button, accentCol);
+//        }
+//    } else {
 #endif
         accentCol = QColor::fromRgb((isLight ? 0xC400C8FF : 0xC4003296) & 0x00FFFFFF);
         pal.setColor(QPalette::Button, accentCol);
 #ifdef Q_OS_WIN
-    }
+//    }
 #endif
 
     if (isLight) {
